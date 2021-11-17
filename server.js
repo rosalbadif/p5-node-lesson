608f15fd5c57583a5dfa825760c7be4fce8cd6a5
rosalbadif <difonzorosalba@gmail.com>
2021-11-17 11:33:45 +0100

    added sketch

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 //will contain all the information that will run on our server 
 console.log("up and running")
+
+let express = require("express") //loaded express
+
+let app= express() //activated express
+
+//port thet we will use to 
+let port = 3000
+
+let server = app.listen(port)
+
+console.log("Server is running on http://localhost:" + port)
+
+
+app.use(express.static("public")) //all the file in the folder won't change over time 
